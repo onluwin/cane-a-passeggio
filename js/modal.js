b35d5d20031c8e0066231e0597e797a1d9c29014
onluwin <nikitagurzhiy1@gmail.com>
2022-08-16 22:07:32 +0300

    .

--- js/modal.js
+++ js/modal.js
@@ -15,37 +15,22 @@
   }
 })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-2]"),
-    closeModalBtn: document.querySelector("[data-modal-close-2]"),
-      modal: document.querySelector("[data-modal]"),
-    body: document.querySelector("body"),
-  };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
-  }
-})();
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open-3]"),
+//     closeModalBtn: document.querySelector("[data-modal-close-3]"),
+//       modal: document.querySelector("[data-modal]"),
+//     body: document.querySelector("body"),
+//   };
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-3]"),
-    closeModalBtn: document.querySelector("[data-modal-close-3]"),
-      modal: document.querySelector("[data-modal]"),
-    body: document.querySelector("body"),
-  };
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
-  }
-})();
+//   function toggleModal() {
+//       refs.modal.classList.toggle("is-hidden");
+//       refs.body.classList.toggle("no-scroll");
+//   }
+// })();
 
