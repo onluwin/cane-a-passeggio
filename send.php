41f37c087178acf8999fc714c10507ac67199de9
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-08-18 14:19:41 +0200

    awd

--- send.php
+++ send.php
@@ -1,23 +1,4 @@
 
-<?php
-$fio = $_POST['nome'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("soulline0@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: trendshoops@gmail.com \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
-
-
 
 <?php
 
