f54e5c4bbc78b754d4f3abf7ab888a9f160e7683
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-08-18 14:16:11 +0200

    999

--- send.php
+++ send.php
@@ -19,3 +19,38 @@ if (mail("soulline0@gmail.com", "Заявка с сайта", "ФИО:".$fio.". 
 
 
 
+<?php
+
+$name = $_POST['nome'];
+$phone = $_POST['numero_di_telefono'];
+$servizio = $_POST['servizio'];
+$un_commento = $_POST['un_commento'];
+$email = $_POST['email'];
+$token = "5474010313:AAGVV-bx5w_hG9vyYDLj2jm6hIoXrDIF90M";
+$chat_id = "-732983981";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+<<<<<<< HEAD
+  'servizio:' => $servizio,
+=======
+  'Servizio:' => $servizio,
+>>>>>>> ad57fc9de03972f098f4e6832cec9edce41e312a
+'Un_commento:' => $un_commento,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: thankpage.html');
+} else {
+  echo "Error";
+}
+
+?>
+/
