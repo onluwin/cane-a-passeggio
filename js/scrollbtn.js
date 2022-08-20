660939d06cd57f3e0a7d9269cf0c1e868a32f85d
onluwin <nikitagurzhiy1@gmail.com>
2022-08-20 14:19:43 +0300

    scrollbtn, adap tablet

---
+++ js/scrollbtn.js
@@ -0,0 +1,16 @@
+mybutton = document.getElementById("scrollbtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
